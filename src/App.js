891768f1998033c2b15c8ae2d140d7fc9b5e46c3
Kristian Elset Bø <kristian.e.boe@gmail.com>
2017-12-26 21:43:58 +0100

    fixed bug where you could add infinite numbering

--- src/App.js
+++ src/App.js
@@ -8,13 +8,11 @@ class App extends Component {
     super();
     this.state = {
       activeItem: 'home',
-      activeStep: 'paste',
       input: '',
       output: [],
       bullets: true,
     }
     this.handleMenuClick = this.handleMenuClick.bind(this);
-    this.handleStepClick = this.handleStepClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.toNumbers = this.toNumbers.bind(this);
@@ -24,7 +22,6 @@ class App extends Component {
   }
   
   handleMenuClick = (e, { name }) => this.setState({ activeItem: name })
-  handleStepClick = (e, { name }) => this.setState({ activeStep: name })
 
   handleChange(e) {
     this.setState({
@@ -42,32 +39,38 @@ class App extends Component {
   }
 
   toNumbers() {
-    const output = this.state.output;
-    
-    let output_numbered = [];
-    for(let i = 0; i < output.length; i++) {
-      output_numbered.push((i+1).toString() + " " + output[i]);
+    if(this.state.bullets) {
+      const output = this.state.output;
+      
+      let output_numbered = [];
+      for(let i = 0; i < output.length; i++) {
+        output_numbered.push((i+1).toString() + " " + output[i]);
+      }
+  
+      this.setState({
+        'output': output_numbered,
+        'bullets': !this.state.bullets
+      })
     }
-
-    this.setState({
-      'output': output_numbered,
-    })
 
   }
 
   toBullets() {
-    const output = this.state.output;
-
-    let output_bullets = [];
-
-    for(let i = 0; i < output.length; i++) {
-      let item_bullet = output[i].split(" ").slice(1).join(" ")
-      
-      output_bullets.push(item_bullet)
+    if(!this.state.bullets) {
+      const output = this.state.output;
+  
+      let output_bullets = [];
+  
+      for(let i = 0; i < output.length; i++) {
+        let item_bullet = output[i].split(" ").slice(1).join(" ")
+        
+        output_bullets.push(item_bullet)
+      }
+      this.setState({
+        'output': output_bullets,
+        'bullets': !this.state.bullets
+      })
     }
-    this.setState({
-      'output': output_bullets,
-    })
   }
 
   handleReverse() {
@@ -88,24 +91,25 @@ class App extends Component {
           <Menu.Menu position='right'>
             <Menu.Item name='logout' active={this.activeItem === 'logout'} onClick={this.handleItemClick} />
           </Menu.Menu>
+          onClick={() => {this.setState({bullets: !this.state.bullets})}}
         </Menu> */}
         <Grid container stackable centered columns={2}>
+          <h1>Reverse my list</h1>
           <Grid.Row>
-            <h1>Reverse my list</h1>
-          </Grid.Row>
           <Grid.Column>
             <Segment raised clearing>
               <h2>Input</h2>
               <Form>
                 <TextArea name="input" placeholder='Paste a list in here' style={{ minHeight: 200 }} onChange={this.handleChange} />
               </Form>
+              <Button onClick={this.handleSubmit}>Convert</Button>
             </Segment>
           </Grid.Column>
           <Grid.Column>
             <Segment raised clearing>
               <h2>Output</h2>
               <List items={this.state.output} />
-              <Button.Group floated="left" onClick={() => {this.setState({bullets: !this.state.bullets})}}>
+              <Button.Group floated="left" >
                 <Button positive={this.state.bullets} onClick={this.toBullets}>Bullets</Button>
                 <Button.Or />
                 <Button positive={!this.state.bullets} onClick={this.toNumbers}>Numbers</Button>
@@ -113,13 +117,14 @@ class App extends Component {
               <Button floated="right" onClick={this.handleReverse}>Reversed</Button>
             </Segment>
           </Grid.Column>
+          </Grid.Row>
         </Grid>
         <Divider hidden />
         <Container>
           <Step.Group fluid stackable='tablet'>
-            <Step active={this.state.activeStep === 'paste'} onClick={this.handleStepClick}  title='Paste' description='Paste your list on the left' />
-            <Step active={this.state.activeStep === 'convert'} onClick={this.handleSubmit}  title='Convert' description='Click me to convert your list' />
-            <Step active={this.state.activeStep === 'copy'} onClick={this.handleStepClick}  title='Copy' description='Copy your new list back to your application' />
+            <Step title='Paste' description='Paste your list on the left' />
+            <Step title='Convert' description='Convert your list' />
+            <Step title='Copy' description='Copy your new list back to your application' />
           </Step.Group>
         </Container>
       </div>
